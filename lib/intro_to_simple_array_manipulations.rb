de301766c60cbc62332082fa0d2091c59877bf94
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-01 16:27:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,6 @@ def using_concat(array_1, array_2)
   array_3 = array_1.concat(array_2)
 end
 
-def using_insert(array, element)
-  
+def using_insert(array_1, element)
+  array_2 = array_1.insert(4, element)
+end
