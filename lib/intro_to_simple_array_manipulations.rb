c4ac0d7af7931507e08a8dc3adb7af6fdde0c623
juliochavesnyc <juliochavesnyc@gmail.com>
2020-12-01 16:54:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ def using_flatten(instruments)
 end
 
 def using_delete(array, string)
-  array = ["hello", "welcome", "goodbye"]
+  array = ["Josh", "Steven", "Sophie", "Steven"]
   string = "hello"
   array.delete(string)
 end
